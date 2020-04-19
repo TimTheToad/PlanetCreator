706eb13659fed351723885d4a049fd6917647f35
Adrian <adde--97@hotmail.com>
2020-04-19 18:19:52 +0200

    Added alternate button for radial menu

--- Sandboxes/RadialController.gd
+++ Sandboxes/RadialController.gd
@@ -56,12 +56,6 @@ func _input(event):
 		if event.is_action_pressed("ui_cancel"):
 			buttonGroup.visible = false
 		
-		if event.is_action_pressed("Radial") and !event.is_action_released("Radial"):
-			buttonGroup.visible = true
-			radialReleased = false
-			PositionButtonsInRadial(get_viewport().get_mouse_position())
-		elif !event.is_action_pressed("Radial"):
-			radialReleased = true
 	pass
 
 func _process(delta):
