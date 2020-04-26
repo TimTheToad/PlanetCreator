e6adc4df26fa19b97505c046a130dbcae6cffceb
TimTheToad <tim.b.mellander@gmail.com>
2020-04-26 11:40:44 +0200

    Moving arrows now work with both camera views

--- Scripts/PlanetMover.gd
+++ Scripts/PlanetMover.gd
@@ -12,25 +12,28 @@ var camera
 func _ready():
 	# Retrive camera
 	cameraHolder = get_tree().current_scene.get_node("CameraHolderMaster")
+
 	if !cameraHolder:
 		print("Failed to retrive cameraHolder for \"PlanetMover.gd\" on node: ", self.name)
 	pass # Replace with function body.
 
+func getZValue(arrow):
+	return camera.transform.origin.distance_to(arrow)
+	pass
 
 func _input(event):
 	
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.is_pressed():
 			# Get essential data for raycast
-			camera = cameraHolder.getCurrentCamera()
 			var viewport = get_viewport()
 			var mousePos = viewport.get_mouse_position()
-			
+			camera = cameraHolder.getCurrentCamera()
 			# Project ray
 			var rayOrigin = camera.project_ray_origin(mousePos)
 			var rayDir = camera.project_ray_normal(mousePos)
 			var rayDist = 1000
-			
+
 			var from = rayOrigin
 			var to = rayOrigin + rayDir * rayDist
 			var spaceState = viewport.get_world().get_direct_space_state()
@@ -47,10 +50,12 @@ func _input(event):
 					selected = null
 					print("zArrow Hit")
 				else:
+					print("Planet Hit")
 					if prevSelected:
 						prevSelected.makeOrbitArrowVisible(false)
 					selected.makeOrbitArrowVisible(true)
 			elif hit.size() == 0:
+				print("missed")
 				if prevSelected:
 					prevSelected.makeOrbitArrowVisible(false)
 
@@ -66,13 +71,13 @@ func _input(event):
 
 	if xArr:
 		if event is InputEventMouseMotion:
-#			var relative = event.get_relative() * 0.1
-			var arrowPosition = camera.project_position(event.position, camera.global_transform.origin.y) - Vector3(1.0, 0.0, 0.0)
+			var distanceToObject = getZValue(xArr.global_transform.origin)
+			var arrowPosition = camera.project_position(event.position, distanceToObject) - Vector3(1.0, 0.0, 0.0)
 			xArr.get_parent().major_axis = arrowPosition.x
 			xArr.get_parent().updateOrbitLines()
 	elif zArr:
 		if event is InputEventMouseMotion:
-#			var relative = event.get_relative() * 0.1
-			var arrowPosition = camera.project_position(event.position, camera.global_transform.origin.y)  - Vector3(0.0, 0.0, 1.0)
+			var distanceToObject = getZValue(zArr.global_transform.origin)
+			var arrowPosition = camera.project_position(event.position, distanceToObject)  - Vector3(0.0, 0.0, 1.0)
 			zArr.get_parent().minor_axis = arrowPosition.z
 			zArr.get_parent().updateOrbitLines()
