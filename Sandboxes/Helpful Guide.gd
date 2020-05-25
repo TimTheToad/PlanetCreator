2d0745b7c42082513c0fa006f648cedf4e77022d
Björn Johansson <andromeda_stargate@hotmail.com>
2020-05-25 03:07:13 +0200

    Added Icons in the HelpText aswell as fixed a few naming inconsistencies

--- Sandboxes/Helpful Guide.gd
+++ Sandboxes/Helpful Guide.gd
@@ -13,11 +13,11 @@ var currentCamera
 var chosenText = 1
 # 0 empty, 1 first, 2 double clicked planet, 3 radial planet, 4 radial planet layers, 5 radial planet fill, 6 radial planet pattern 
 var textArr = ["", "Double click any planet to orbit around it and begin editing that planet!", 
-"Click \"Add Layer\" to add one of the three avalable layers: Water, Ground and Lava \nClick on \"Advanced\" to get to the blueprint editor \nClick on \"Alternate View\" to open up a window with a closeup of the current planet \nFinaly click on the back arrow to exit out from the planet editing menu",
-"Click on one of the three icons to add the corresponding layer on the planet \"Fire\" for Lava, \"Water\" for Water and \"Ground\" for Ground",
-"Click on the \"Bucket\" icon to fill the planet with this chosen layer \nClick on the \"Pattern\" icon to add a pattern of chosen layer \nClick on \"...\" to choose a new action to pin to the radial menu around the planet \n",
-"Click on the Square Color to choose a color for this layer \nClick on the green checkmark to apply the layer or the \"backarrow\" to undo the layer and return to the previous menu",
-"Click on the Square Color to choose a color for this layer \nDrage the \"Period\" dragger to change the size of the pattern \nDrag the \"Octave\" dragger to change the hardness of the pattern \nClick on the green checkmark to apply the layer or the \"backarrow\" to undo the layer and return to the previous menu",
+"Click \"Add Layer\" to add one of the three avalable layers: Water, Ground and Lava \nClick on \"Advanced\" to get to the blueprint editor \nClick on \"Alternate View\" to open up a window with a closeup of the current planet \nFinaly click on  \"[img=20x10]res://Assets/Icons/redo.png[/img]\" to exit out from the planet editing menu",
+"Click on one of the three icons to add the corresponding layer on the planet \"[img=20x20]res://Assets/Icons/LayerIcons/Lava.png[/img]\" for Lava, \"[img=20x10]res://Assets/Icons/LayerIcons/Liquid.png[/img]\" for Water and \"[img=20x10]res://Assets/Icons/LayerIcons/Base.png[/img]\" for Ground \nTo go back to the previous menu click on \"[img=20x10]res://Assets/Icons/redo.png[/img]\"",
+"Click on the \"[img=20x20]res://Assets/Icons/EventIcons/fill.png[/img]\" icon to fill the planet with this chosen layer \nClick on the \"[img=20x20]res://Assets/Icons/EventIcons/noise.png[/img]\" icon to add a pattern of chosen layer \nClick on \"...\" to choose a new action to pin to the radial menu around the planet \n",
+"Click on the Square Color to choose a color for this layer \nClick on the \"[img=20x10]res://Assets/Icons/done.png[/img]\" to apply the layer or the \"[img=20x10]res://Assets/Icons/redo.png[/img]\" to undo the layer and return to the previous menu",
+"Click on the Square Color to choose a color for this layer \nDrage the \"Quality Distribution\" dragger to change the size of the pattern \nDrag the \"Detail\" dragger to change the hardness of the pattern \nClick on the \"[img=20x10]res://Assets/Icons/done.png[/img]\" to apply the layer or the \"[img=20x10]res://Assets/Icons/redo.png[/img]\" to undo the layer and return to the previous menu",
 ]
 
 
